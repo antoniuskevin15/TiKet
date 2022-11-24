0710e57b14f53bd719b9513cdeceeb573e2f805b
Dope-Bit <62684521+Darren-Lionardo@users.noreply.github.com>
2022-11-24 20:19:44 +0700

    24/11/2022

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import './Login.css';
 
 const Home: React.FC = () => {
   return (
