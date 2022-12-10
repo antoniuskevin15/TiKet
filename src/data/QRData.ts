baec1dfb23f48eb09952b723306c0b0ea7555daa
antoniuskevin15 <antoniuskevin15@gmail.com>
2022-12-10 15:24:52 +0700

    update: Not Needed Code

--- src/data/QRData.ts
+++ src/data/QRData.ts
@@ -5,14 +5,16 @@ export interface QRData {
     data: string,
 };
 
-export interface CTX {
-    qrCode: QRData[];
-    addQR: (newQR: QRData) => void,
-    updateQR: (updateQR: QRData) => void,
-}
 
-const QRCode = React.createContext<CTX>({
-    qrCode: [],
-    addQR: () => {},
-    updateQR: () => {}
-});
+// NOT NEEDED
+// export interface CTX {
+//     qrCode: QRData[];
+//     addQR: (newQR: QRData) => void,
+//     updateQR: (updateQR: QRData) => void,
+// }
+
+// const QRCode = React.createContext<CTX>({
+//     qrCode: [],
+//     addQR: () => {},
+//     updateQR: () => {}
+// });
