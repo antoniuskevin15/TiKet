14328abf70fdd78c3a34331bf7d323665fa7c0c8
antoniuskevin15 <antoniuskevin15@gmail.com>
2022-12-19 18:45:11 +0700

    done build new android

--- android/app/src/main/java/io/ionic/tiket/kelompok1/MainActivity.java
+++ android/app/src/main/java/io/ionic/tiket/kelompok1/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package io.ionic.tiket.kelompok1;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'tiket',
+  appId: 'io.ionic.tiket.kelompok1',
+  appName: 'TikeT',
   webDir: 'build',
   bundledWebRuntime: false
 };
